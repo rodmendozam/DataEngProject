51028bf6d17c67596069b1e9dead12e3d389e1db
Rodrigo Mendoza <rmendoza@mendozablanco.com.mx>
2016-03-16 15:39:24 +0100

    on click buttons show structure and content

--- controller_interface.js
+++ controller_interface.js
@@ -8,7 +8,17 @@ app_graph.controller('myCtrl', function($scope) {
 
     angular.element(document).ready(function () {
 
+        $("#form_content").hide();
+        $("#form_structure").hide();
         //$("#intro").css("background-color", "yellow");
+        $("#structure_metrics_button").click(function(){
+            $("#form_structure").show();
+            $("#form_content").hide();
+        });
+        $("#content_metrics_button").click(function(){
+            $("#form_structure").hide();
+            $("#form_content").show();
+        });
 
     });
 
